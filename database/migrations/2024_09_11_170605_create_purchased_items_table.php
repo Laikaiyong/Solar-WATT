fb8f8130a63c405a74cf209d821f28a2724b01b2
Berryen <fooaishen@yahoo.com>
2024-09-12 03:06:28 +0800

    feat/add customer tables

---
+++ database/migrations/2024_09_11_170605_create_purchased_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchased_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained('purchase')->onDelete('cascade');
+            $table->foreignId('solar_product_id')->constrained('solar_products_services')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchased_items');
+    }
+};
